a47d87ca040134829786319a4ce8dd95c49f8452
Doug Osborn <d00303905@dmail.dixie.edu>
2021-02-15 17:12:04 -0700

    debug

--- main.ts
+++ main.ts
@@ -1,15 +1,21 @@
+let piny = 0
+let pinx = 0
 let x = 0
 let y = 0
 basic.forever(function () {
+    pinx = pins.analogReadPin(AnalogPin.P1)
+    piny = pins.analogReadPin(AnalogPin.P2)
+    basic.showNumber(pinx)
+    basic.pause(200)
     x = pins.map(
-    pins.analogReadPin(AnalogPin.P0),
-    150,
+    pinx,
+    0,
     800,
     0,
     4
     )
     y = pins.map(
-    pins.analogReadPin(AnalogPin.P2),
+    piny,
     150,
     800,
     0,
